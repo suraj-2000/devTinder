d9248be0ae1e28172a679ae35f26e94264f293fc
Suraj Singh <suraj_bt18@iiitkalyani.ac.in>
2026-05-09 02:04:09 +0530

    practice http methods get, post, put, delete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //Imports the Express.js library
+const app = express(); //Calls the Express function,Creates your server object (app)
 
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello Dashboard");
+    
+// });
 
 app.use("/hello",(req,res)=> {
-    res.send("Hello Dosto");
+    res.send("Hello Hello Hello");
 });
 
-app.use((req,res)=>{
-    res.send("Hello Dashboard");
-    
+app.get("/user",(req,res)=>{
+    res.send({"name":"Suraj Singh",
+        "age": "25"
+    });
 });
+
+app.post("/user",(req,res)=>{
+    res.send("user successfully saved!")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("user deleted successfully!")
+})
+
+// app.use("/test",(req,res)=>{
+//     res.send("Test Test Test")
+
+// });
 
 
 
